0b6b7778dcaabdf762cf528b6c1f610838f4be66
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-04-16 08:52:41 -0700

    Continued on our animal farm

--- script/script.js
+++ script/script.js
@@ -7,3 +7,15 @@ console.log('hello world');
 //parameters for constructor- animal name- dog, cat, sheep, or horse
 //property - animalName
 //method to render itself on the screen render()
+
+
+function Animal (animalName) {
+    this.animalName = animalName;
+}
+
+
+Animal.prototype.render() = function() {
+    console.log("this function has been called")
+}
+
+//when user clicks on the cat button, I want a cat to be created- under the cat button
