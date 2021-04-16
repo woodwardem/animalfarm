32780e90c4d865ce6c4ae9125282acc9ad461fba
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-04-16 08:11:12 -0700

    animal farm day 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # animalfarm
 this is the project repo for our animal farm display
+
+
+- We need some kind of comtainers to represent the circles
+- Need an h1 to the title 
+- Probably need a big container to hold all the animals- "farm"
---
+++ script/script.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+
+
+// create a template for an animal 
+// is that four differant templates? 
+// create a single constructor - 
+//parameters for constructor- animal name- dog, cat, sheep, or horse
+//property - animalName
+//method to render itself on the screen render()
