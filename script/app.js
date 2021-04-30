89bd0c59a9022c391e32a4c0f44a53f49d6b4445
woodwardem <woodwardem@g.crookcountyschools.org>
2021-04-30 08:57:49 -0700

    code for animal farm

--- script/app.js
+++ script/app.js
@@ -1,8 +1,9 @@
-//console.log('hello world');
+('use strict')
 
-const form = document.getElementById('FORM') [0];
+const form = document.getElementsByTagName('FORM') [0];
+form.addEventListener('submit', makeNewArticle );
 
-
-function makeNewArticle() {
-    
+function makeNewArticle(event) {
+event.preventDefault();
+console.log('hello, button was clicked')
 }
