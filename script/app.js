3e613dd1297f63ee88d1a26ac2a2ff5e0218e91a
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-05-05 08:21:05 -0700

    code for animal farm

--- script/app.js
+++ script/app.js
@@ -1,8 +1,27 @@
 //console.log('hello world');
 
-const form = document.getElementById('FORM') [0];
+const form = document.getElementsByTagName('FORM') [0];
+
+form.addEventListener('submit', makeNewArticle);
 
 
-function makeNewArticle() {
-    
+
+function makeNewArticle(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const journal = event.target.content.value;
+
+    const article = document.createElement('article');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+
+
+    h2.textContent = title;
+    p.textContent = journal;
+
+    article.appendChild(h2);
+    article.appendChild(p);
+
+    const main = document.querySelector('main')
+    main.appendChild(article);
 }
